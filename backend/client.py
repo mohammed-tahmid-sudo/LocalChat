3d8e92726fd4f6b0ddd681cd417e072a04076271
mohammed-tahmid-sudo <mohammedtahmid.bd@gmail.com>
2025-09-16 23:03:59 +0600

    I use Neovim BTW

--- backend/client.py
+++ backend/client.py
@@ -1,23 +1,6 @@
-import asyncio
+import socket
 
-async def client(name):
-    reader, writer = await asyncio.open_connection('127.0.0.1', 5000)
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    async def send_messages():
-        while True:
-            msg = input(f"{name}: ")
-            writer.write(msg.encode())
-            await writer.drain()
-
-    async def receive_messages():
-        while True:
-            data = await reader.read(1024)
-            if not data:
-                print("Connection closed")
-                break
-            print(f"Received: {data.decode()}")
-
-    await asyncio.gather(send_messages(), receive_messages())
-
-asyncio.run(client("Client1"))  # change to "Client2" for second client
+print("socket Created succesfully")
 
