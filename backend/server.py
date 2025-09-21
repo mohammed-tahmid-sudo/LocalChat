32842b2afce681bc0472299b50a02f8a6be17747
mohammed-tahmid-sudo <mohammedtahmid.bd@gmail.com>
2025-09-21 23:12:38 +0600

    updates

--- backend/server.py
+++ backend/server.py
@@ -1,26 +1,31 @@
-import json
-import socket
+import socket, json, threading
+
+def handle_client(conn, name):
+    while True:
+        try:
+            data = conn.recv(2048)
+            if not data:
+                break
+            data_json = json.loads(data.decode())
+            print(f"Received from {name}: {data_json}")
+        except:
+            break
+    conn.close()
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 soc.bind(("0.0.0.0", 12345))
 soc.listen(2)
+print("Server listening...")
 
 conn1, addr1 = soc.accept()
+print("Client 1 connected:", addr1)
+threading.Thread(target=handle_client, args=(conn1, "Client1"), daemon=True).start()
+
 conn2, addr2 = soc.accept()
+print("Client 2 connected:", addr2)
+threading.Thread(target=handle_client, args=(conn2, "Client2"), daemon=True).start()
 
+# Keep server running
 while True:
-
-    # Receive from client 1
-    data1 = conn1.recv(2048)
-    if data1:
-        data1_json = json.loads(data1.decode())
-        print("Received data from client 1:")
-        print(data1_json)
-
-    # Receive from client 2
-    data2 = conn2.recv(2048)
-    if data2:
-        data2_json = json.loads(data2.decode())
-        print("Received data from client 2:")
-        print(data2_json)
+    pass
 
