7c2e273ec4a5a3149ec585b02348ac8dd3e33689
mohammed-tahmid-sudo <mohammedtahmid.bd@gmail.com>
2025-09-22 14:16:18 +0600

    adding the database

--- backend/client.py
+++ backend/client.py
@@ -1,5 +1,8 @@
 import socket
 import json
+from datetime import datetime, timezone
+
+ts = datetime.now(timezone.utc).isoformat(timespec='seconds')
 
 # connect to server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,11 +11,13 @@ client.connect(("127.0.0.0", 12345))  # replace with server IP
 while True:
     # example data to send
     data = {
-        "name": input("Enter name: "),
-        "age": int(input("Enter age: "))
+        # "action": input("ENTER YOUR MESSAGE: "),
+        "id": "123456789",
+        "sender": input("NAME: "),
+        "content": input("content: "),
+        "timestamp": ts
     }
 
     # convert dict to JSON string and encode to bytes
     client.sendall(json.dumps(data).encode())
     print("Data sent to server.")
-
--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,36 @@
-import socket, json, threading
+import socket
+import json
+import threading
+import sqlite3
+
+db = sqlite3.connect("/home/tahmid/LocalChat/backend/data/data.db")
+
+if db:
+    print("DataBase Created or It was ALready Created ")
+c = db.cursor()
+c.execute("""CREATE TABLE IF NOT EXISTS users (
+                id TEXT PRIMARY KEY,
+                username TEXT UNIQUE
+            )""")
+c.execute("""CREATE TABLE IF NOT EXISTS rooms (
+                id TEXT PRIMARY KEY,
+                name TEXT
+            )""")
+c.execute("""CREATE TABLE IF NOT EXISTS room_members (
+                room_id TEXT,
+                user_id TEXT,
+                PRIMARY KEY(room_id, user_id)
+            )""")
+c.execute("""CREATE TABLE IF NOT EXISTS messages (
+                id TEXT PRIMARY KEY,
+                room_id TEXT,
+                sender_id TEXT,
+                content TEXT,
+                type TEXT DEFAULT 'text',
+                timestamp TEXT
+            )""")
+db.commit()
+
 
 def handle_client(conn, name):
     while True:
@@ -11,6 +43,7 @@ def handle_client(conn, name):
         except:
             break
     conn.close()
+
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 soc.bind(("0.0.0.0", 12345))
