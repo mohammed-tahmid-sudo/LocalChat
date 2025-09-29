6605feaa62e406952bd930992fcab995fdc8d434
mohammed-tahmid-sudo <mohammedtahmid.bd@gmail.com>
2025-09-29 23:23:40 +0600

    updates

--- backend/client.py
+++ backend/client.py
@@ -1,83 +1,74 @@
 import socket
-# import os
 import threading
 import time
-import json 
+import json
+
+IS_USER_FOUND = False
+
+
+######################################################
+
+def send_ping(sock, id):
+
+    while True:
+        try:
+            data = {
+                "type":"ping", 
+                "id": id
+            }
+            sock.sendall(json.dumps(data).encode())
+        except Exception as e:
+            print(e)
+        time.sleep(5)
+
+
+######################################################
+
 try:
-    with open("/home/tahmid/LocalChat/backend/userdata.json", 'r') as d: 
+    with open("/home/tahmid/LocalChat/backend/userdata.json", "r") as d:
         userdata = json.load(d)
-    print(userdata)
+        IS_USER_FOUND = True
+
 
 except FileNotFoundError as e:
 
-    print("welcome!!\n") 
+    print("welcome!!\n")
     print("first Enter your name:")
     username = input()
     print("you're all set. maybe just for now!")
 
-    data = {
-        "user_create": {
-            "name": username
-        }
-    }
+    data = {"name": username}
 
-    with open("/home/tahmid/LocalChat/backend/userdata.json", 'w') as f:
-        json.dump(data, f, indent=10000000)
+    with open("/home/tahmid/LocalChat/backend/userdata.json", "w") as f:
+        json.dump(data, f, indent=8)
+
+######################################################
 
 
-    
+def if_datafound():
+    if IS_USER_FOUND:
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn.connect(("127.0.0.1", 12345))
+
+        t = threading.Thread(target=send_ping, args=(conn, userdata["name"]))
+        t.daemon = True
+        t.start()
+
+        while True:
+            try:
+                msg = conn.recv(1024).decode()
+                # if not msg:
+                #     break
+                print("Received:", msg)
+            except Exception as e:
+                print(e)
+
+
+
+if __name__ == '__main__':
+    if_datafound()
 
 
 
 
 
-
-
-
-# def send_ping(sock):
-#
-#     # ping_message = {
-#     #     "username": "Tahmid",
-#     #     "ping": True,
-#     # }
-#
-#     while True:
-#         try:
-#             sock.sendall(b"PING")
-#         except:
-#             break
-#         time.sleep(5)
-#
-#
-# def main():
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     sock.connect(("127.0.0.1", 12345))
-#
-#
-#
-#
-#     # Start ping thread
-#     threading.Thread(target=send_ping, args=(sock,), daemon=True).start()
-#
-#     # Main loop for receiving & sending custom messages
-#     while True:
-#         try:
-#             data = sock.recv(1024)
-#             if not data:
-#                 break
-#             print("Received:", data.decode())
-#
-#             # Example: send something else (not just ping)
-#             user_input = input("Enter message: ")
-#             sock.sendall(user_input.encode())
-#
-#         except KeyboardInterrupt:
-#             break
-#         except:
-#             break
-#
-#     sock.close()
-#
-#
-# if __name__ == "__main__":
-#     main()
--- backend/server.py
+++ backend/server.py
@@ -1,55 +1,58 @@
+######################################################################################################
 import socket
 import time
 import json
 import sqlite3
 import threading
 
+######################################################################################################
 
-def initialize():
-    conn = sqlite3.connect("/home/tahmid/LocalChat/backend/data/users.db")
-    cursor = conn.cursor()
 
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            lastseen TEXT
-            )
-        """
-    )
-    conn.commit()
-    conn.close()
+# def initialize():
+#     conn = sqlite3.connect("/home/tahmid/LocalChat/backend/data/users.db")
+#     cursor = conn.cursor()
+#
+#     cursor.execute(
+#         """
+#         CREATE TABLE IF NOT EXISTS users (
+#             id INTEGER PRIMARY KEY,
+#             name TEXT NOT NULL,
+#             lastseen TEXT
+#             )
+#         """
+#     )
+#     conn.commit()
+#     conn.close()
+#
+#     conn2 = sqlite3.connect("/home/tahmid/LocalChat/backend/data/holder.db")
+#     cursor2 = conn2.cursor()
+#
+#     cursor2.execute(
+#         """
+#         CREATE TABLE IF NOT EXISTS users (
+#
+#             sender_id TEXT,
+#             reciver_id TEXT
+#             message TEXT
+#
+#         )
+#
+#         """
+#     )
+#     conn2.commit()
+#     conn2.close()
 
-    conn = sqlite3.connect("/home/tahmid/LocalChat/backend/data/holder.db")
 
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS users (
-
-            sender_id TEXT, 
-            reciver_id TEXT
-            message TEXT
-    
-        )
-
-        """
-    )
-    conn.commit()
-    conn.close()
-
+######################################################################################################
 
 def handle_client(conn, addr):
-        
+
     db = sqlite3.connect("/home/tahmid/LocalChat/backend/data/users.db")
-    cursor = conn.cursor()
-    db.commit()
-    db.close()
-
-
-
+    cursor = db.cursor()
 
     print(f"Connected by {addr}")
+
+######################################################################################################
     while True:
         try:
             data = conn.recv(2048)
@@ -63,26 +66,57 @@ def handle_client(conn, addr):
 
             print(f"From {addr}: {msg}")
 
-            if "ping" in msg:
-                cursor.execute("""
+######################################################################################################
+
+            if msg["type"] == "create_user":
+
+                cursor.execute(
+                    """
+                    INSERT INTO users (name, lastseen) VALUES (?, ?)
+
+                """,
+                    (msg["name"], time.time()),
+                )
+                db.commit()
+
+                data = {"type": "user_id", "id": cursor.lastrowid}
+
+                conn.sendall(json.dumps(data).encode())
+
+######################################################################################################
+
+            elif msg["type"] == "ping":
+
+                cursor.execute(
+                    """
                     UPDATE users 
-                    SET lastseen = ?,
+                    SET lastseen = ?
                     WHERE id = ?
 
-                """, (time.time() ,msg["ping"]["id"]))
-            
+                """,
+                    (time.time(), msg["id"]),
+                )
+                db.commit()
+
+######################################################################################################
 
             else:
                 conn.sendall(b"ACK: " + data)
 
-        except:
-            break
-    conn.close()
+######################################################################################################
+
+        except Exception as e:
+            print(e)
+
+    db.close
     print(f"Disconnected {addr}")
 
 
+######################################################################################################
+
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind(("0.0.0.0", 12345))
     server.listen()
     print("Server listening on 0.0.0.0:12345")
@@ -92,6 +126,10 @@ def main():
         threading.Thread(target=handle_client, args=(conn, addr), daemon=True).start()
 
 
+######################################################################################################
+
 if __name__ == "__main__":
-    initialize()
+    # initialize()
     main()
+
+######################################################################################################
